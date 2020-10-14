058467f69d20fc8885f90844ec9c9b3e660f1eee
ing <ingafter60@gmail.com>
2020-10-14 09:05:57 +0700

    4.10 Add verbose_name and verbose_name_plural

--- rooms/models.py
+++ rooms/models.py
@@ -22,28 +22,32 @@ class RoomType(AbstractItem):
 
     """ RoomType Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"   
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
 
 
 class Room(core_models.TimeStampedModel):
