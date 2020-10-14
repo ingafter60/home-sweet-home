65071986b1c17c3870f5da533e7f36fba5560212
ing <ingafter60@gmail.com>
2020-10-14 09:36:10 +0700

    4.16 Register Photo to admin and add 'notes' to each of registered class

--- README.md
+++ README.md
@@ -34,6 +34,7 @@
 	> 4.11 Make amenities, facilities, and house_rules fields blank true
 	> 4.12 Create Photo class and add relationship with room
 	> 4.15 Use string for class  name
-	
+	> 4.16 Register Photo to admin and add 'notes' to each of registered class
+
 
 	
--- rooms/admin.py
+++ rooms/admin.py
@@ -9,10 +9,23 @@ from . import models
 	models.Amenity, 
 	models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    """ Room Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ """
 
     pass
